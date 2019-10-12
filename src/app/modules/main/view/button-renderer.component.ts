7226566483d0ac560c31990e0728b590de39f24d
madhukar chouhan <madhukarsingh11@gmail.com>
2019-10-12 17:14:04 +0530

    delete Row

--- src/app/modules/main/view/button-renderer.component.ts
+++ src/app/modules/main/view/button-renderer.component.ts
@@ -13,6 +13,7 @@ template:`<button mat-icon-button  [matMenuTriggerFor]="menu">
     <mat-icon>more_vert</mat-icon>
   </button>
 <mat-menu #menu="matMenu">
+  <button mat-menu-item (click)="onClick($event)">View</button>
   <button mat-menu-item (click)="onClick($event)">Edit</button>
   <button mat-menu-item (click)="deleteRow($event, index)">Delete</button>
 </mat-menu>`
@@ -21,12 +22,10 @@ template:`<button mat-icon-button  [matMenuTriggerFor]="menu">
 
 export class ButtonRendererComponent implements ICellRendererAngularComp {
    params;
-
   agInit(params): void {
     this.params = params;
   }
   constructor(private mainService:MainService){
-
   }
 
   refresh(params?: any): boolean {
@@ -39,8 +38,9 @@ export class ButtonRendererComponent implements ICellRendererAngularComp {
     
   }
 
-  deleteRow(params, index){
-     this.mainService.removeItem(params, index).subscribe(data => {
+  deleteRow($events, index){
+     this.params;
+     this.mainService.removeItem(this.params, index).subscribe(data => {
             alert('Success');
           })
      }
